21248446ff7586e060f0fbe18ef5824018d03e3d
Andrew <andrew.jones@and.digital>
2024-02-06 16:26:16 +0000

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,69 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import * as contentful from 'contentful';
+import { Router } from "next/router";
 
-export default function Home() {
+export default async function Home() {
+
+  const components = await getContent();
+  console.log(components);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+   <div>{components}</div>
   );
+}
+
+const getContent = async () => {
+  const client = contentful.createClient({
+    space: process.env.SPACE as string,
+    accessToken: process.env.DELIVERY_KEY as string
+  });
+const entries = await client.getEntries({ content_type: 'page', limit: 10 })
+
+const components = convertToReactComponents(entries.items);
+console.log(components);
+
+return components;
+}
+
+const convertToReactComponents = (entries: any) => {
+
+  let homepage = {fields: {elements: [], sys: {contentType: {sys: {id: ''}}}}};
+  for (const entry of entries) {
+    if (entry.fields.slug === 'home') {
+      homepage = entry;
+    }
+  }
+  const components: any[] = [];
+  for (const el of homepage.fields.elements) {
+    console.log(el.sys.contentType.sys.id)
+    switch (el.sys.contentType.sys.id) {
+      case 'textElement':
+        components.push(
+          textElementTransformer(el.fields)
+        );
+        break;
+
+      default:
+        break;
+    }
+  }
+  return components;
+}
+
+const textElementTransformer = (fields: any) => {
+  const {type, content, amplifiers, top, left} = fields;
+
+  switch (type) {
+    case 'h1':
+      return <h1 style={{position: 'fixed', top: top + 'px', left: left + 'px'}}>{content}</h1>;
+    case 'h2':
+      return <h2>{content}</h2>;
+    case 'h3':
+      return <h3>{content}</h3>;
+    case 'body':
+      return <div>{content}</div>;
+    case 'small':
+      return <small>{content}</small>;
+    default:
+      return <div>{content}</div>;
+  }
 }
