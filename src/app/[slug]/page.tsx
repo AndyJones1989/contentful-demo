b6860f3fdbc57de17e20b32aada41e75c8d4336b
Andrew <andrew.jones@and.digital>
2024-02-07 14:07:14 +0000

    renderer complete

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,47 +1,44 @@
 import Image from "next/image";
-import * as contentful from 'contentful';
+import * as contentful from "contentful";
 
-export default async function Home({params}) {
-
-  const components = await getContent();
-  return (
-   <div>{components}</div>
-  );
+export default async function Home({ params }: { params: any }) {
+  const components = await getContent(params.slug);
+  return <div>{components}</div>;
 }
 
-const getContent = async () => {
+const getContent = async (params: string) => {
   const client = contentful.createClient({
     space: process.env.SPACE as string,
-    accessToken: process.env.DELIVERY_KEY as string
+    accessToken: process.env.DELIVERY_KEY as string,
   });
-const entries = await client.getEntries({ content_type: 'page', limit: 10 })
+  const entries = await client.getEntries({ content_type: "page", limit: 10 });
 
-const components = convertToReactComponents(entries.items);
+  const components = convertToReactComponents(entries.items, params);
 
-return components;
-}
+  return components;
+};
 
-const convertToReactComponents = (entries: any) => {
-
-
-  let homepage = {fields: {elements: [], sys: {contentType: {sys: {id: ''}}}}};
+const convertToReactComponents = (entries: any, params: string) => {
+  let page = {
+    fields: { elements: [], sys: { contentType: { sys: { id: "" } } } },
+  };
   for (const entry of entries) {
-    if (entry.fields.slug === 'home') {
-      homepage = entry;
+    if (entry.fields.slug === params) {
+      page = entry;
     }
   }
+  if (page.fields.elements.length === 0) {
+    return <div>Page not found</div>;
+  }
   const components: any[] = [];
-  for (const el of homepage.fields.elements) {
-    switch (el.sys.contentType.sys.id) {
-      case 'textElement':
-        components.push(
-          textElementTransformer(el.fields)
-        );
+  for (const el of page.fields.elements) {
+    let element = el as contentful.Entry<any>;
+    switch (element.sys.contentType.sys.id) {
+      case "textElement":
+        components.push(textElementTransformer(element.fields));
         break;
-      case 'image':
-        components.push(
-          imageElementTransformer(el.fields)
-        );
+      case "image":
+        components.push(imageElementTransformer(element.fields));
         break;
 
       default:
@@ -49,30 +46,47 @@ const convertToReactComponents = (entries: any) => {
     }
   }
   return components;
-}
+};
 
 const textElementTransformer = (fields: any) => {
-  const {type, content, amplifiers, top, left} = fields;
+  const { type, content, amplifiers, top, left } = fields;
 
   switch (type) {
-    case 'h1':
-      return <h1 key='yo' style={{position: 'fixed', top: top + 'px', left: left + 'px'}}>{content}</h1>;
-    case 'h2':
+    case "h1":
+      return (
+        <h1
+          key="yo"
+          style={{ position: "relative", top: top + "%", left: left + "%" }}
+        >
+          {content}
+        </h1>
+      );
+    case "h2":
       return <h2>{content}</h2>;
-    case 'h3':
+    case "h3":
       return <h3>{content}</h3>;
-    case 'body':
+    case "body":
       return <div>{content}</div>;
-    case 'small':
+    case "small":
       return <small>{content}</small>;
     default:
       return <div>{content}</div>;
   }
-}
+};
 
 const imageElementTransformer = (fields: any) => {
-  const {image, width, height, top, left} = fields;
+  const { image, width, height, top, left } = fields;
   return (
-  <div key={height} style={{position: 'fixed', top: top + 'px', left: left + 'px'}}><Image src={'https:'+image.fields.file.url} width={width} height={height} alt={image.fields.id} /></div>
-  )
-}
+    <div
+      key={height}
+      style={{ position: "relative", top: top + "%", left: left + "%" }}
+    >
+      <Image
+        src={"https:" + image.fields.file.url}
+        width={width}
+        height={height}
+        alt={image.fields.id}
+      />
+    </div>
+  );
+};
